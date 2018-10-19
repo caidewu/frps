0d0508f55b2b8b870caa16faf95a164e4a494b4c
caidewu <caidewu555@gmail.com>
2018-10-19 14:03:10 +0800

    更新下载链接

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # frp内网穿透服务端自动运行脚本
 
-> 该项目fork https://github.com/clangcn/onekey-install-shell，由于onekey-install-shell这个项目名字语意不明，所以这里新建了一个项目
+> 该项目fork [clangcn/onekey-install-shell](https://github.com/clangcn/onekey-install-shell)，由于onekey-install-shell这个项目名字语意不明，所以这里新建了一个项目
 
 > 项目基于[fatedier/frp](https://github.com/fatedier/frp)
 
@@ -9,13 +9,13 @@
 
 > 该项目是运行在外网服务器上，以下的所有操作确保是在外网服务器中进行
 
-- 下载脚本
+### 下载脚本
 
 ```
 wget --no-check-certificate https://raw.githubusercontent.com/caidewu/frps/master/install.sh -O ./install.sh
 ```
 
-- 安装
+### 安装
 
 ```
 # 安装过程会有一系列去要确人的配置信息，没有特殊要求用default的就好了
@@ -43,13 +43,13 @@ Log file           : enable
 ==============================================
 ```
 
-- 测试服务（以上面配置信息为例）
+### 测试服务（以上面配置信息为例）
 
 用浏览器可以直接访问：`http://100.100.100.100:7500`, (确保外网服务器安全组的7000，7500端口是打开的)
 
 ![frps管理后台](/example1.png)
 
-- 其他常用命令
+### 其他常用命令
 
 ```
 frps {start|stop|restart|status|config|version}
--- install.sh
+++ install.sh
@@ -13,8 +13,8 @@ str_program_dir="/usr/local/${program_name}"
 program_init="/etc/init.d/${program_name}"
 program_config_file="frps.ini"
 ver_file="/tmp/.frp_ver.sh"
-program_version_link="https://raw.githubusercontent.com/clangcn/onekey-install-shell/master/frps/version.sh"
-str_install_shell=https://raw.githubusercontent.com/clangcn/onekey-install-shell/master/frps/install-frps.sh
+program_version_link="https://raw.githubusercontent.com/caidewu/frps/master/version.sh"
+str_install_shell=https://raw.githubusercontent.com/caidewu/frps/master/install.sh
 shell_update(){
     fun_clangcn "clear"
     echo "Check updates for shell..."
@@ -180,11 +180,11 @@ fun_get_version(){
     fi
 }
 fun_getServer(){
-    def_server_url="aliyun"
+    def_server_url="github"
     echo ""
     echo -e "Please select ${program_name} download url:"
-    echo -e "[1].aliyun (default)"
-    echo -e "[2].github"
+    echo -e "[1].aliyun"
+    echo -e "[2].github(default)"
     read -p "Enter your choice (1, 2 or exit. default [${def_server_url}]): " set_server_url
     [ -z "${set_server_url}" ] && set_server_url="${def_server_url}"
     case "${set_server_url}" in
